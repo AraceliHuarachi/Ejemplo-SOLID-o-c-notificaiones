402d866540dd1c9ac69d027e131f6784b4e89c7e
Araceli HT <araceli@pull.srl>
2024-11-19 13:14:44 -0400

    examen calculadora de salarios

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CalculatorController;
 use App\Http\Controllers\NotificationController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/send-notification', [NotificationController::class, 'sendNotification']);
+Route::get('/calculator', [CalculatorController::class, 'calculator']);
